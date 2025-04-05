4d2e699a1a21817cb32b06592565f7b574c329ce
Timur Gilfanov <timurgilfanov@gmail.com>
2025-04-05 15:46:11 +0400

    Refactor: Rename Greeting composable function to greeting
    - Rename `Greeting` composable function to `greeting`.

--- app/src/main/java/timur/gilfanov/guacamole/MainActivity.kt
+++ app/src/main/java/timur/gilfanov/guacamole/MainActivity.kt
@@ -40,7 +40,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
 @Preview(showBackground = true)
 @Composable
-private fun GreetingPreview() {
+fun GreetingPreview() {
     VigilantguacamoleTheme {
         Greeting("Android")
     }
